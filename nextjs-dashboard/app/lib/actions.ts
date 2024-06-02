3a346a73ce8ab4a4bcf645ba02059db488098c74
MisikIvan <vt211_miv@student.ztu.edu.ua>
2024-06-02 20:52:15 +0300

    Chapter 13

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,11 +6,42 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
  
 
-export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+
+  export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    try {
+      await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+   
     revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
   }
 
+  export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+   
+    // Unreachable code block
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+      revalidatePath('/dashboard/invoices');
+      return { message: 'Deleted Invoice' };
+    } catch (error) {
+      return { message: 'Database Error: Failed to Delete Invoice' };
+    }
+  }
 
 const FormSchema = z.object({
   id: z.string(),
