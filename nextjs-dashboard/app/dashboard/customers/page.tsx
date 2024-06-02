bd319600398616a2cceb72b928be35ab1a41de6c
MisikIvan <vt211_miv@student.ztu.edu.ua>
2024-06-02 21:16:02 +0300

    Chapter 16

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customer',
+};
+
+export default function CustomerPage() {
     return <p>Customers Page</p>;
-  }
+}
